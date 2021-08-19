8c26613fd31be8c9703fd2e7999c1fcbb1ac784c
ljq <luojq@citydo.com.cn>
2021-08-19 14:06:40 +0800

    eslint校验

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    mode: 'production',
-    entry: {
-        main: './src/index.js',
-    },
-    output: {
-        filename: 'vue-identidy-comp.js',
-        library: 'inentifyComp'
-    },
-    devServer: {
-        port: 9000,
-        open: true
-    },
-    plugins: [
-        // new HtmlWebpackPlugin({
-        //     template: './public/index.html'
-        // })
-    ],
-    module: {
-        rules: [{
-            exclude: /src/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    plugins: ["@babel/plugin-transform-runtime"]
-                }
-            }
-        }]
-    }
+  mode: 'production',
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: 'vue-identidy-comp.js',
+    library: 'inentifyComp'
+  },
+  devServer: {
+    port: 9000,
+    open: true
+  },
+  plugins: [
+    // new HtmlWebpackPlugin({
+    //     template: './public/index.html'
+    // })
+  ],
+  module: {
+    rules: [{
+      exclude: /src/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          plugins: ['@babel/plugin-transform-runtime']
+        }
+      }
+    }]
+  }
 }
